87928ab56083371f7c23b109879d6fb7e9632c17
tiago <tiago.rainho@ua.pt>
2021-10-25 19:46:11 +0100

    change files input

--- src/Generator.py
+++ src/Generator.py
@@ -4,19 +4,18 @@ class Generator:
 
     def __init__(self, fcm: FCM):
         self.fcm = fcm
-
     
-    def generate(self, text:str = None):
+    def generate(self, text:str = None) -> str:
         last_characters = text
         if text != None:
             while True:
-                next_character = self._get_next(last_characters[-self.fcm.k:])
+                next_character = self._get_next(last_characters[-self.fcm.k:])                
                 if next_character == None: break
-                last_characters = last_characters + next_character
+                last_characters = last_characters[-self.fcm.k-1:] + next_character
                 yield next_character
         return None
 
-    def _get_next(self, last_characters):
+    def _get_next(self, last_characters:str):
         tuple_list = self.fcm.finitecontext.get(last_characters)
         if tuple_list != None:
             return max(tuple_list, key=lambda t: t[1])[0]
--- src/aula01.py
+++ src/aula01.py
@@ -1,19 +1,22 @@
 import sys
 from Generator import Generator
 from FCM import FCM
-import time
+import time, string
 
-def main(k, alpha, text):
+def main(k, alpha):
+
+    files = ['example/example3.txt', 'example/example4.txt']
 
     fcm = FCM(k, alpha)
     #fcm.load_alphabet("ACGT")
 
-    fcm.update(text)
-    #fcm.update("abcdefghijklmnopqrstuvxzABCDEFGHIJKLMNOPQRSTUVXZ")
+    for file in files:
+        add_file(file ,fcm)
+
     fcm.update("From Romeo and Juliet to King Lear to Macbeth to all of his stunning sonnets and other works, William Shakespeare’s top spot in the literary world has been solidified for centuries. Although his work is well-covered in many schools, famous Shakespeare quotes do not only belong in English courses for study. We can all appreciate his tongue-in-cheek observations about society and individuals’ behavior, as well as gain inspiration and wisdom from his quick-witted quips and smart sense of humor. Not only that, he can help us find beauty through language—including an appreciation for vulnerable declarations of love.")
     
     generator = Generator(fcm)
-    text = "O livro O Principezinho fala de um menino que vivia num planeta"
+    text = "famous Shakespeare quotes"
 
     returned_text = generator.generate(text)
 
@@ -25,13 +28,21 @@ def main(k, alpha, text):
             break
         print(next_char, end='', flush=True)
         time.sleep(0.1)
-    
+
+def add_file(file, fcm):
+    with open('../' + file) as f:
+        file_str = f.read().replace('\n', '')#.lower()
+        fcm.update(file_str)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: python3 aula01.py <file_path> <k> <alpha>")
-        
-    f = open(sys.argv[1], 'r')
-    file_str = f.read().replace('\n', '')
 
-    main(int(sys.argv[2]), int(sys.argv[3]), file_str)
+    if len(sys.argv) != 3:
+        print("Usage: python3 aula01.py <k> <alpha>")
+
+    #if len(sys.argv) != 4:
+    #    print("Usage: python3 aula01.py <file_path> <k> <alpha>")
+        
+    #f = open(sys.argv[1], 'r')
+    #file_str = f.read().replace('\n', '')
+
+    main(int(sys.argv[1]), int(sys.argv[2]))
